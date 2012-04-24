70da2cd8808f9fd638cf123bfb8f84c5a4b75c02
AntonPetrov <anton.i.petrov@gmail.com>
2012-04-23 21:06:37 -0400

    Work on qunit testing

--- JAR3Doutput/JAR3Dresults/static/js/jar3dInputValidator.js
+++ JAR3Doutput/JAR3Dresults/static/js/jar3dInputValidator.js
@@ -1,25 +1,152 @@
 
 var jar3dInputValidator = {
 
-    hasSecondaryStructure: function( input ) {
+    splitLines: function( input ) {
+        var lines;
+        if ( input.match(/\r\n|\r|\n/) ) {
+            lines = input.split(/\r\n|\r|\n/)
+        } else {
+            lines = input;
+        }
+        for (var i = 0; i < lines.length-1; i++) {
+            lines[i].trim();
+        }
+        return lines;
+    },
 
-        if ( input.match(/(|)|\./) ) {
+    // line is a fasta-formatted description line
+    isFastaLine: function( line ) {
+        return line[0] == '>' ? true : false;
+    },
+
+    // line is a valid loop
+    isLoopLine: function( line ) {
+
+        var self = this,
+            L = line.length,
+            MAX_LOOP_LENGTH = 25,
+            validChars = line.split(/A|C|G|U|T|\*/ig).length - 1,
+            chainBreaks = line.split(/\*/g).length - 1;
+
+        if ( L < MAX_LOOP_LENGTH &&
+             L == validChars &&
+             chainBreaks < 2 &&
+             this.complementaryClosingBases( line ) ) {
             return true;
         } else {
             return false;
         }
+    },
+
+    complementaryClosingBases: function( line ) {
+        var self = this,
+            pairs = self.getClosingBases( line );
+
+        var valid = $.grep(pairs, function(pair, i) {
+            return self.isComplementaryPair( pair );
+        });
+
+        return valid.length == pairs.length;
+    },
+
+    getClosingBases: function( line ) {
+        var closingBases = new Array,
+            starPosition = line.indexOf('*');
+
+        // the first and the last bases
+        closingBases.push(line[0] + line[line.length-1]);
+
+        if ( starPosition > 0 ) {
+            closingBases.push(line[starPosition-1] + line[starPosition+1]);
+        }
+        return closingBases;
+    },
+
+    isComplementaryPair: function( pair ) {
+        return $.inArray(pair, ['AU', 'UA', 'GC', 'CG', 'GU', 'UG']) < 0 ? false : true;
+    },
+
+    // line is a valid nucleotide sequence
+    isSequenceLine: function( line ) {
+        var L = line.length,
+            validChars = line.split(/A|C|G|U|T|-/ig).length - 1,
+            MAX_SEQUENCE_LENGTH = 500;
+
+        if ( L <= MAX_SEQUENCE_LENGTH &&
+             validChars.length == L) {
+            return true;
+         } else {
+            return false;
+         }
 
     },
 
-    hasFasta: function( input ) {
+    // line is a valid secondary structure
+    isSecondaryStructure: function( line ) {
+        var open  = line.split(/\(/g).length - 1,
+            close = line.split(/\)/g).length - 1,
+            dots  = line.split(/\./g).length - 1;
+        return open + close + dots == line.length && open == close;
+    },
 
-        if ( input.match(/^>/) ) {
-            return true;
-        } else {
+    // individual functions for each input case
+    isFastaSingleLoop: function( input ) {
+        var self = this,
+            lines = self.splitLines(input);
+        return lines.length == 2 &&
+               self.isFastaLine( lines[0] ) &&
+               self.isLoopLine( lines[1] );
+    },
+
+    isNoFastaSingleLoop: function( input ) {
+        var self = this,
+            loop = self.splitLines(input);
+
+        return typeof(loop) == 'string' &&
+               self.isLoopLine( loop );
+    },
+
+    /*
+        Multiple loops with fasta descriptions
+        >seq1
+        GAAAC*GUAGC
+        >seq2
+        GACUC*GUAGC
+    */
+    isFastaMultipleLoops: function( input ) {
+        var self = this,
+            lines = self.splitLines(input),
+            l = lines.length,
+            fasta = new Array(),
+            loops = new Array(),
+            badloops = new Array();
+
+        // uneven number of lines or less than 4 lines
+        if ( l % 2 != 0 || l < 4 ) {
             return false;
         }
 
-    }
+        $.each(lines, function(i, line) {
+            // 0, 2, 4 => fasta
+            // 1, 3, 5 => loop
+            if ( i % 2 ) {
+                loops.push(line);
+                return self.isLoopLine(line);
+            } else {
+                fasta.push(line);
+                return self.isFastaLine(line);
+            }
+        });
 
+        // all loops should be the same length
+        badLoops = $.grep(loops, function(loop, i) {
+            return loop.length != loops[0].length;
+        });
+
+        return fasta.length == loops.length &&
+               fasta.length == lines.length / 2 &&
+               badLoops.length == 0;
+
+    }
 
 };
--- JAR3Doutput/JAR3Dresults/static/js/tests.js
+++ JAR3Doutput/JAR3Dresults/static/js/tests.js
@@ -1,71 +1,94 @@
 var testCases = {
 
     // extracted loops
-    fastaSingleLoop: '>seq1\
-GAAAC*GGACC',
+    fastaSingleLoop: ['>seq1','GAAAC*GGACC'].join('\n'),
 
     nofastaSingleLoop : 'GAAAC*GGACC',
 
-    fastaMultipleLoops: '>seq1\
-GAAC*GGACC\
->seq2\
-GAAC*GGACC\
->seq3\
-GAAC*GGACC',
+    fastaMultipleLoops: ['>seq1', 'GAAC*GGACC',
+                         '>seq2', 'GAAC*GGACC',
+                         '>seq3', 'GAAC*GGACC'].join('\n'),
 
-    nofastaMultipleLoops: 'GAAAC*GGACC\
-GAAAC*GGACC\
-GAAAC*GGACC\
-GAAAC*GGACC',
+    nofastaMultipleLoops: ['GAAAC*GGACC',
+                           'GAAAC*GGACC',
+                           'GAAAC*GGACC',
+                           'GAAAC*GGACC'].join('\n'),
 
     // one entire sequence
-    fastaSingleSequenceSS: '(((((.....))))))\
->seq1\
-AAAAAAAAAAAAAAAAAAAAAA',
+    fastaSingleSequenceSS: ['(((((.....))))))',
+                            '>seq1',
+                            'AAAAAAAAAAAAAAAAAAAAAA'].join('\n'),
 
-    nofastaSingleSequenceSS: '((((((((((())))))))))))\
-AAAAAAAAAAAAAAAAAAAAAA',
+    nofastaSingleSequenceSS: ['((((((((((())))))))))))',
+                              'AAAAAAAAAAAAAAAAAAAAAA'].join('\n'),
 
-    fastaSingleSequenceNoSS: '>seq1\
-AAAAAAAAAAAAAAAAAAAAAA',
+    fastaSingleSequenceNoSS: ['>seq1','AAAAAAAAAAAAAAAAAAAAAA'].join('\n'),
 
     nofastaSingleSequenceNoSS: 'AAAAAAAAAAAAAAAAAAAAAA',
 
     // multiple sequences
-    fastaMultipleSequencesSS: '(((((......)))))))\
->seq1\
-AAAAAAAAAAAAAAAAAA\
->seq2\
-UUUUUUUUUUUUUUUUUU\
->seq3\
-GGGGGGGGGGGGGGGGGG',
+    fastaMultipleSequencesSS: ['(((((......)))))))',
+                               '>seq1', 'AAAAAAAAAAAAAAAAAA',
+                               '>seq2', 'UUUUUUUUUUUUUUUUUU',
+                               '>seq3', 'GGGGGGGGGGGGGGGGGG'].join('\n'),
 
-    nofastaMultipleSequencesSS: '(((((((((.....)))))))\
-AAAAAAAAAAAAAAAAA\
-GGGGGGGGGGGGGGGGG\
-UUUUUUUUUUUUUUUUU',
+    nofastaMultipleSequencesSS: ['(((((((((.....)))))))',
+                                 'AAAAAAAAAAAAAAAAA',
+                                 'GGGGGGGGGGGGGGGGG',
+                                 'UUUUUUUUUUUUUUUUU'].join('\n'),
 
-    fastaMultipleSequencesNoSS: '>seq1\
-AAAAAAAAAAA\
->seq2\
-GGGGGGGGGGG\
->seq3\
-CCCCCCCCCCC',
+    fastaMultipleSequencesNoSS: ['>seq1','AAAAAAAAAAA',
+                                 '>seq2','GGGGGGGGGGG',
+                                 '>seq3','CCCCCCCCCCC'].join('\n'),
 
-    nofastaMultipleSequencesNoSS: 'AAAAAAAAAAA\
-GGGGGGGGGGG\
-UUUUUUUUUUU'
+    nofastaMultipleSequencesNoSS: ['AAAAAAAAAAA',
+                                   'GGGGGGGGGGG',
+                                   'UUUUUUUUUUU'].join('\n'),
 
 };
 
+var validator = jar3dInputValidator;
 
-
-test( "hello test", function() {
-    ok( 1 == "1", "Passed!" );
+test( "isLoopLine", function() {
+    ok(validator.isLoopLine('CCGU*ACGUG'), 'Valid internal loop');
+    ok(validator.isLoopLine('CCGUACGUG'), 'Valid hairpin loop');
+    equal(validator.isLoopLine('CCGUACGUGAUCGGUAUGUCUGAGUAUGUCGUAGC'), false, 'Very long loop');
+    equal(validator.isLoopLine('CAGU*ACGA*G'), false, 'Extra chain break character');
+    equal(validator.isLoopLine('CA&$GU*ACGAHG'), false, 'Disallowed characters');
+    equal(validator.isLoopLine('CAGU*UCGAG'), false, 'Complementary internal loop');
+    equal(validator.isLoopLine('AGUUCGAG'), false, 'Complementary hairpin');
 });
 
-test( "Fasta tests", function() {
-    ok( jar3dInputValidator.hasFasta(testCases['fastaSingleLoop']), 'Passed');
-    equal( jar3dInputValidator.hasFasta(testCases['nofastaSingleLoop']), false, 'Passed');
+test( "isSecondaryStructure", function() {
+    ok(validator.isSecondaryStructure('(((...)))'), 'Valid secondary structure');
+    equal(validator.isSecondaryStructure('(((((....))'), false, 'Unequal number of ( and )');
+    equal(validator.isSecondaryStructure('(((((.N..)))))'), false, 'Disallowed characters');
+});
+
+test( "isFastaSingleLoop", function() {
+    ok( validator.isFastaSingleLoop(testCases['fastaSingleLoop']), 'Passed');
+    $.each(testCases, function(type, i) {
+        if ( type != 'fastaSingleLoop' ) {
+            equal( validator.isFastaSingleLoop(testCases[type]), false, testCases[type]);
+        }
+    });
+});
+
+test( "isNoFastaSingleLoop", function() {
+    ok( validator.isNoFastaSingleLoop(testCases['nofastaSingleLoop']) );
+    $.each(testCases, function(type, i) {
+        if ( type != 'nofastaSingleLoop' ) {
+            equal( validator.isNoFastaSingleLoop(testCases[type]), false, testCases[type]);
+        }
+    });
+});
+
+test( "isFastaMultipleLoops", function() {
+    ok( validator.isFastaMultipleLoops(testCases['fastaMultipleLoops']) );
+    $.each(testCases, function(type, i) {
+        if ( type != 'fastaMultipleLoops' ) {
+            equal( validator.isFastaMultipleLoops(testCases[type]), false, testCases[type]);
+        }
+    });
 });
 
