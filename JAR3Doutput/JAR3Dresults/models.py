78406d3f6551b21e546ebedf6cac475eb5ba3ab6
Blake Sweeney <blakes.85@gmail.com>
2013-03-06 15:41:06 -0500

    Make models more pythonic.

--- JAR3Doutput/JAR3Dresults/models.py
+++ JAR3Doutput/JAR3Dresults/models.py
@@ -1,69 +1,74 @@
-# This is an auto-generated Django model module.
-# You'll have to do the following manually to clean this up:
-#     * Rearrange models' order
-#     * Make sure each model has one field with primary_key=True
-# Feel free to rename the models, but don't rename db_table values or field names.
-#
-# Also note: You'll have to insert the output of 'django-admin.py sqlcustom [appname]'
-# into your database.
+from django.db.models import Model
+from django.db.models import CharField
+from django.db.models import TextField
+from django.db.models import DecimalField
+from django.db.models import IntegerField
+from django.db.models import DateTimeField
+from django.db.models import SmallIntegerField
 
-from django.db import models
 
-class Query_info(models.Model):
-    query_id = models.CharField(max_length=36, primary_key=True, db_index=True)
-    group_set = models.CharField(max_length=20, default='default') #motifs release
-    model_type = models.CharField(max_length=20, default='default') #bph_stack etc
-    query_type = models.CharField(max_length=35)
-    structured_models_only = models.IntegerField(default=0)
-    email = models.CharField(blank=True, max_length=100)
-    status = models.IntegerField(default=0)
-    parsed_input = models.TextField()
-    time_submitted = models.DateTimeField(auto_now_add=True)
-    time_completed = models.DateTimeField(blank=True, null=True)
+class QueryInfo(Model):
+    query_id = CharField(max_length=36, primary_key=True, db_index=True)
+    group_set = CharField(max_length=20, default='default')
+    model_type = CharField(max_length=20, default='default')
+    query_type = CharField(max_length=35)
+    structured_models_only = IntegerField(default=0)
+    email = CharField(blank=True, max_length=100)
+    status = IntegerField(default=0)
+    parsed_input = TextField()
+    time_submitted = DateTimeField(auto_now_add=True)
+    time_completed = DateTimeField(blank=True, null=True)
+
     class Meta:
         db_table = u'jar3d_query_info'
 
-class Query_sequences(models.Model):
-    query_id = models.CharField(max_length=36, db_index=True)
-    seq_id = models.SmallIntegerField()
-    loop_id = models.SmallIntegerField()
-    loop_type = models.CharField(max_length=2)
-    loop_sequence = models.TextField()
-    internal_id = models.CharField(max_length=8)
-    user_seq_id = models.TextField()
-    status = models.SmallIntegerField(default=0)
-    time_submitted = models.DateTimeField(auto_now_add=True)
-    time_completed = models.DateTimeField(blank=True, null=True)
+
+class QuerySequences(Model):
+    query_id = CharField(max_length=36, db_index=True)
+    seq_id = SmallIntegerField()
+    loop_id = SmallIntegerField()
+    loop_type = CharField(max_length=2)
+    loop_sequence = TextField()
+    internal_id = CharField(max_length=8)
+    user_seq_id = TextField()
+    status = SmallIntegerField(default=0)
+    time_submitted = DateTimeField(auto_now_add=True)
+    time_completed = DateTimeField(blank=True, null=True)
+
     class Meta:
         db_table = u'jar3d_query_sequences'
 
-class Results_by_loop_instance(models.Model):
-    query_id = models.CharField(max_length=36, db_index=True)
-    seq_id = models.SmallIntegerField()
-    loop_id = models.SmallIntegerField()
-    motif_id = models.CharField(max_length=11)
-    score = models.DecimalField(max_digits=10, decimal_places=6)
-    percentile = models.DecimalField(max_digits=10, decimal_places=6)
-    interioreditdist = models.SmallIntegerField()
-    fulleditdist = models.SmallIntegerField()
-    rotation = models.SmallIntegerField()
+
+class ResultsByLoopInstance(Model):
+    query_id = CharField(max_length=36, db_index=True)
+    seq_id = SmallIntegerField()
+    loop_id = SmallIntegerField()
+    motif_id = CharField(max_length=11)
+    score = DecimalField(max_digits=10, decimal_places=6)
+    percentile = DecimalField(max_digits=10, decimal_places=6)
+    interioreditdist = SmallIntegerField()
+    fulleditdist = SmallIntegerField()
+    rotation = SmallIntegerField()
+
     class Meta:
         db_table = u'jar3d_results_by_loop_instance'
 
-class Results_by_loop(models.Model):
-    query_id = models.CharField(max_length=36, db_index=True)
-    loop_id = models.SmallIntegerField()
-    motif_id = models.CharField(max_length=11)
-    meanscore = models.DecimalField(max_digits=10, decimal_places=6)
-    meanpercentile = models.DecimalField(max_digits=10, decimal_places=6)
-    meaninterioreditdist = models.DecimalField(max_digits=10, decimal_places=6)
-    meanfulleditdist = models.DecimalField(max_digits=10, decimal_places=6)
-    medianscore = models.DecimalField(max_digits=10, decimal_places=6)
-    medianpercentile = models.DecimalField(max_digits=10, decimal_places=6)
-    medianinterioreditdist = models.DecimalField(max_digits=10, decimal_places=6)
-    medianfulleditdist = models.DecimalField(max_digits=10, decimal_places=6)
-    signature = models.CharField(max_length=150)
-    rotation = models.SmallIntegerField()
-    correspondences = models.TextField()
+
+class ResultsByLoop(Model):
+    query_id = CharField(max_length=36, db_index=True)
+    loop_id = SmallIntegerField()
+    motif_id = CharField(max_length=11)
+    meanscore = DecimalField(max_digits=10, decimal_places=6)
+    meanpercentile = DecimalField(max_digits=10, decimal_places=6)
+    meaninterioreditdist = DecimalField(max_digits=10, decimal_places=6)
+    meanfulleditdist = DecimalField(max_digits=10, decimal_places=6)
+    medianscore = DecimalField(max_digits=10, decimal_places=6)
+    medianpercentile = DecimalField(max_digits=10, decimal_places=6)
+    medianinterioreditdist = DecimalField(max_digits=10, decimal_places=6)
+    medianfulleditdist = DecimalField(max_digits=10, decimal_places=6)
+    signature = CharField(max_length=150)
+    rotation = SmallIntegerField()
+    correspondences = TextField()
+
     class Meta:
         db_table = u'jar3d_results_by_loop'
