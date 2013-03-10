8d259c5b86d377eb163cb7e96c951056e78a90a5
Blake Sweeney <blakes.85@gmail.com>
2013-03-09 21:05:25 -0500

    Reorganize all static files.
    The static files have been moved around to be all in a single consistent
    structure. In addition I added some new js files.

---
+++ JAR3Doutput/JAR3Dresults/static/js/jar3d.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+  $("#results").tablesorter();//{
+    //headers: {
+      //1: {
+        //textExtraction: function(node) {
+
+        //}
+      //}
+    //}
+  //})
+});
