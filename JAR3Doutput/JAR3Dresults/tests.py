5583250b0345c44e0186f27e98587fdb2f85c636
jimroll <jroll@bgsu.edu>
2014-05-12 10:06:07 -0400

    Testing clean up

--- JAR3Doutput/JAR3Dresults/tests.py
+++ JAR3Doutput/JAR3Dresults/tests.py
@@ -18,18 +18,38 @@ class SimpleTest(TestCase):
 
 class FungalSeqTest(TestCase):
     def setUp(self):
-	    self.ss = '..((((...((((((.......((...........((((..((((.....(((((.....(((((...((((((.......)))))).......((((((((((((((....((((((...))))))))((((..(((.(((.(((((((.....((.((((((.......(((((....))))).)))))).)).)).......))))))))....)))..)))).))))).)))).)))(((((.(((...((((((((((((((.........................(((((((((((.((..(((......))))))))))))))))(((((((((...((((((.....))))))...))))))).))((((((......))))))))))))))).............)))))...)))))))).......................)))))........)))))....))))..)))).))......))))))))))....((((....((((..........................((.(((..(((.(((..........)))......)))..))).))....))))....))))......................'
-	    self.seq = ['GUGAGU---GACCUCU-UCUUUCCU-CUACUCGUUUCGCACCCCACCA-CCCAAUUUUCUGGGGGCUCUCUCCAA-CCUUAUGGAGAACCCCAAACCCGCACUGCGUCUGUU-CUCGACAAUCGA-GACGUUCUUCCACCCGUGACGAGG------UCCGCGCGCG-UUUUCGAUCGCUUGAUCGCCGCGUGGA--CCGCUGCAUACGUCCGGUC-AUGGUUGGACGCGCGGUUGUGUGGUGAAAUGGGGGGCUGAGCGAGGAGUGCGGAAAAGAC-AGCAGUAUAUCCCUCUGGGAGCUG-------------------------------CAUGGUUGGAUAACCGACUGCACUCGCUGUGACGCAGUUUGAGUGGCAUGUC---GGCGAUGCACUUUUUA------CUCGAUUCACGUAGCCCCCUGUGUAACCGUUUUCAGCGUUGUACCCGUUGAU---------------UGGGACACAGACAACGCGCCACCUUCCAUGUUCGAGCG------------------------------CACGCUGACAUUUUGGUCUCUAUAUCCUU--------------CUCAUCCCUCGAAUUGGAUCGUUG--------CAUAAUGUAAG']
-
-    def test_fungal_seq(self):
+        self.ss = '..((((...((((((.......((...........((((..((((.....(((((.....(((((...((((((.......)))))).......((((((((((((((....((((((...))))))))((((..(((.(((.(((((((.....((.((((((.......(((((....))))).)))))).)).)).......))))))))....)))..)))).))))).)))).)))(((((.(((...((((((((((((((.........................(((((((((((.((..(((......))))))))))))))))(((((((((...((((((.....))))))...))))))).))((((((......))))))))))))))).............)))))...)))))))).......................)))))........)))))....))))..)))).))......))))))))))....((((....((((..........................((.(((..(((.(((..........)))......)))..))).))....))))....))))......................'
+        self.seq = ['GUGAGU---GACCUCU-UCUUUCCU-CUACUCGUUUCGCACCCCACCA-CCCAAUUUUCUGGGGGCUCUCUCCAA-CCUUAUGGAGAACCCCAAACCCGCACUGCGUCUGUU-CUCGACAAUCGA-GACGUUCUUCCACCCGUGACGAGG------UCCGCGCGCG-UUUUCGAUCGCUUGAUCGCCGCGUGGA--CCGCUGCAUACGUCCGGUC-AUGGUUGGACGCGCGGUUGUGUGGUGAAAUGGGGGGCUGAGCGAGGAGUGCGGAAAAGAC-AGCAGUAUAUCCCUCUGGGAGCUG-------------------------------CAUGGUUGGAUAACCGACUGCACUCGCUGUGACGCAGUUUGAGUGGCAUGUC---GGCGAUGCACUUUUUA------CUCGAUUCACGUAGCCCCCUGUGUAACCGUUUUCAGCGUUGUACCCGUUGAU---------------UGGGACACAGACAACGCGCCACCUUCCAUGUUCGAGCG------------------------------CACGCUGACAUUUUGGUCUCUAUAUCCUU--------------CUCAUCCCUCGAAUUGGAUCGUUG--------CAUAAUGUAAG']
         validator = views.JAR3DValidator()
         fasta = []
-        (loops, indices) = validator.isfolded_extract_loops(self.ss,self.seq)
-        query_info = validator.make_query_info('1','1','1')
-        query_seqs = validator.make_query_sequences(loops, fasta, '1')
-        query_inds = validator.make_query_indices(indices,'1')
-        [seq.save() for seq in query_seqs]
-        query_seqs_db = Query_sequences.objects.filter(query_id='1')
-        self.assertEqual(len(loops.keys()),35)
-        self.assertEqual(len(query_seqs),35)
-        self.assertEqual(len(query_seqs_db),35)
+        (self.loops, self.indices) = validator.isfolded_extract_loops(self.ss,self.seq)
+        self.query_info = validator.make_query_info('1','1','1')
+        self.query_seqs = validator.make_query_sequences(self.loops, fasta, '1')
+        self.query_inds = validator.make_query_indices(self.indices,'1')
+        [seq.save() for seq in self.query_seqs]
+        self.query_seqs_db = Query_sequences.objects.filter(query_id='1')
+    def test_fungal_ss_reader(self):    
+        self.assertEqual(len(self.loops.keys()),35)
+    def test_fungal_dict_convert(self):    
+        self.assertEqual(len(self.query_seqs),35)
+    def test_fungal_db_saver(self):    
+        self.assertEqual(len(self.query_seqs_db),35)
+
+class ViroidSeqTest(TestCase):
+    def setUp(self):
+        self.ss = '.((((((((.((.((((((((.(.((((.((((((..(((((.((((((...(((........(((((((((((.((...((((((((..((((((((.....(((((((((.(.((((...((((..(((((((.(((.((..((((.((.((((((.....((((..((((.(((((....)))))...))))..)))).....))))).))).)))).))..))).))))))).))))))).).))))))))))......))))).)))..)..))))))))))))))..)))))).........)))...)).)))).)))).).))))))..)))))..)))))))))))))))))).'
+        self.seq = ['CGGAACUAAACUCGUGGUUCCUGUGGUUCACACCUGACCUCCUGAGCAGAAAAGA-AAAAAGAAGGCGGCUCGGAGGAGCGCUUCAGGGAUCCCCGGGGAAACCUGGAGCGAACUGGCA-AUAAGGACGGUGGGGAGUGCCC-AGCGGCCGACAGGAGUAAUUCCCGCCGAAACAGGGUUUUCACCCUUCCUUUCUUCGGGUGUCCUUCCUCGCGCCCGCAGGACCACCCCUCGCCCCCUUUGCGCUGUCGCUUCGGAUAUUACCCGGUGGAAACAACUGAAGCUCCCGAGAACCGCUUUUUCUC-UAUCUUUCUUUGCUUCGGGGCGAGGGUGUUUAGCCCUUGGAACCGCAGUUGGUUCCU']
+        validator = views.JAR3DValidator()
+        fasta = []
+        (self.loops, self.indices) = validator.isfolded_extract_loops(self.ss,self.seq)
+        self.query_info = validator.make_query_info('1','1','1')
+        self.query_seqs = validator.make_query_sequences(self.loops, fasta, '1')
+        self.query_inds = validator.make_query_indices(self.indices,'1')
+        [seq.save() for seq in self.query_seqs]
+        self.query_seqs_db = Query_sequences.objects.filter(query_id='1')
+    def test_fungal_ss_reader(self):    
+        self.assertEqual(len(self.loops.keys()),34)
+    def test_fungal_dict_convert(self):    
+        self.assertEqual(len(self.query_seqs),34)
+    def test_fungal_db_saver(self):    
+        self.assertEqual(len(self.query_seqs_db),34)
