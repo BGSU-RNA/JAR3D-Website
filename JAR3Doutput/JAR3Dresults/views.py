948d20ec836996767519937a850c3d2b40f38d08
Blake Sweeney <blakes.85@gmail.com>
2014-05-22 10:15:14 -0400

    Update result design
    I've moved the jsmol window up to the top of the loops so we can see all
    buttons. IN addition, I now use pre blocks for the loop and search boxes as well
    as preventing wrapping in these boxes. I've also moved the refine search button
    down to near the summary area.

--- JAR3Doutput/JAR3Dresults/views.py
+++ JAR3Doutput/JAR3Dresults/views.py
@@ -26,8 +26,6 @@ import logging
 import pdb
 import re
 
-# logging.basicConfig(filename="/Users/api/apps/jar3d_dev/logs/django.log", level=logging.INFO)
-#logging.setLevel(logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 def home(request, uuid=None):
