ff2f92007a0bed424df58b9420be2bdfd537091c
jimroll <jroll@bgsu.edu>
2014-05-23 16:14:29 -0400

    Format input for display

--- JAR3Doutput/JAR3Dresults/views.py
+++ JAR3Doutput/JAR3Dresults/views.py
@@ -69,16 +69,16 @@ def result(request, uuid):
 
     if q.status == 1:
         zippedResults = zip(results.loops, results.sequences, results.indices)
-        input_alignment = make_input_alignment(q.parsed_input,q.query_type)
+        #input_alignment = make_input_alignment(q.parsed_input,q.query_type)
         return render_to_response('JAR3Doutput/base_result_done.html',
                                   {'query_info': q, 'num': results.input_stats,
-                                   'results': zippedResults, 'input_alignment': input_alignment},
+                                   'results': zippedResults, 'input_alignment': q.parsed_input},
                                   context_instance=RequestContext(request))
     elif q.status == 0 or q.status == 2:
-    	input_alignment = make_input_alignment(q.parsed_input,q.query_type)
+    	#input_alignment = make_input_alignment(q.parsed_input,q.query_type)
         return render_to_response('JAR3Doutput/base_result_pending.html',
                                   {'query_info': q, 'num': results.input_stats, 
-                                   'input_alignment': input_alignment},
+                                   'input_alignment': q.parsed_input},
                                   context_instance=RequestContext(request))
     else:
         return render_to_response('JAR3Doutput/base_result_failed.html',
