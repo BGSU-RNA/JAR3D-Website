94c9030c59a87013494687e3ec05ea5f7b5a8f51
jimroll <jroll@bgsu.edu>
2014-06-05 17:45:38 -0400

    Format input display

--- JAR3Doutput/JAR3Dresults/views.py
+++ JAR3Doutput/JAR3Dresults/views.py
@@ -69,6 +69,7 @@ def result(request, uuid):
 
     if q.status == 1:
         zippedResults = sort_loops(results.loops, results.indices, results.sequences)
+        q.formatted_input = make_input_alignment(q.parsed_input,q.query_type)
         return render_to_response('JAR3Doutput/base_result_done.html',
                                   {'query_info': q, 'num': results.input_stats,
                                    'results': zippedResults},
@@ -409,3 +410,41 @@ def sort_loops(loops, indices, sequences):
     mins = [ str(x) for x in mins ]
     sorted_lists = sorted(zip(loops, sequences, indices, mins), key = lambda x: int(x[3]))
     return sorted_lists
+
+def make_input_alignment(parsed_input, query_type):
+    #If inut is just one loop, return
+    if query_type[-4] == 'L' or query_type[-5] =='L':
+        return parsed_input
+    #Get info about query
+    query_lines = parsed_input.split('\n')
+    has_ss = True;
+    has_fasta = True;
+    if query_type[2] == 'N':
+        has_fasta = False
+    if query_type[-4] == 'N':
+        has_ss = False
+    #Make formatted alignment for display
+    first_seq_row = 0
+    first_seq_row += 1 if has_ss else first_seq_row
+    first_seq_row += 1 if has_fasta else first_seq_row
+    seq_length = len(query_lines[first_seq_row])
+    i = 1
+    out = ''
+    while i <= seq_length:
+        if i < 10:
+            out += '  ' + str(i) + '  '
+        elif i < 100:
+            out += '  ' + str(i) + ' '
+        else:
+            out += ' ' + str(i) + ' '
+    out += '\n'
+    line = 0
+    if has_ss:
+        out += '  ' + "    ".join(query_lines[line]) + '  \n'
+        line += 1
+    while line < len(query_lines):
+        if has_fasta:
+            out += query_lines[line] + '\n'
+            line += 1
+        out += '  ' + "    ".join(query_lines[line]) + '  \n'
+    return out
