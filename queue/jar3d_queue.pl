00530d88996a78902f0609abac8e102eb8145f06
jimroll <jroll@bgsu.edu>
2014-08-21 08:00:20 -0400

    Increase timeout limit

--- queue/jar3d_queue.pl
+++ queue/jar3d_queue.pl
@@ -22,7 +22,7 @@ my $TERM :shared = 0;
 my $IDLE_QUEUE = Thread::Queue->new();
 
 # CPU time out for each query in seconds
-my $TIMEOUT = 3000;
+my $TIMEOUT = 7200;
 
 # refresh time
 my $SLEEP = 5;
