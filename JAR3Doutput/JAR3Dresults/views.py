b068d13e7910e351b4a43d1c4c67644429aaa299
jimroll <jroll@bgsu.edu>
2014-12-29 12:47:26 -0500

    Update to single loop result view

--- JAR3Doutput/JAR3Dresults/views.py
+++ JAR3Doutput/JAR3Dresults/views.py
@@ -107,7 +107,7 @@ def single_result(request,uuid,loopid,motifgroup):
             seq = Query_sequences.objects.filter(query_id = uuid, seq_id = res.seq_id, loop_id = loopid)[0].loop_sequence
             rows.append(line_base + '_Position_' + str(corr_line.sequence_position) + '_' + 
                 seq[corr_line.sequence_position-1] + ' aligns_to_JAR3D ' + res.motif_id + '_Node_' + str(corr_line.node) + 
-                '_Position_' + str(corr.node_position))
+                '_Position_' + str(corr_line.node_position))
             if corr_line.is_insertion:
                 seq = seq + '_Insertion'
         name = Query_sequences.objects.filter(query_id = uuid, seq_id = res.seq_id, loop_id = loopid)[0].user_seq_id
