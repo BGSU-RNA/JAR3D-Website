eac6fc0688c7631a73f6c246d6b98c2db0eceea5
jimroll <jroll@bgsu.edu>
2015-01-16 14:17:55 -0500

    Update to single results page

--- JAR3Doutput/JAR3Dresults/views.py
+++ JAR3Doutput/JAR3Dresults/views.py
@@ -127,10 +127,22 @@ def single_result(request,uuid,loopid,motifgroup):
     with open(filenamewithpath,"r") as f:
         model_text = f.read()
     header, motifalig, sequencealig = alignsequencesandinstancesfromtext(model_text,instance_text)
+    lines = []
+    lines.append(header['columnname'])
+    lines.append(header['nodes'])
+    lines.append(header['positions'])
+    lines.append(header['insertions'])
+    keys = sorted(motifalig.keys())
+    for key in keys:
+        lines.append(motifalig[key])
+    keys = sorted(sequencealig.keys())
+    for key in keys:
+        lines.append(sequencealig[key])
+    columnum = len(lines[0]
     q = Query_info.objects.filter(query_id=uuid)
     q = q[0]  # We are interested only in the first one
     return render_to_response('JAR3Doutput/base_result_loop_done.html',
-                                  {'query_info': q, 'header': header,  'motifalig': motifalig, 'sequencealig': sequencealig},
+                                  {'query_info': q, 'lines': lines, 'colnum': colnum},
                                   context_instance=RequestContext(request))
 
 
