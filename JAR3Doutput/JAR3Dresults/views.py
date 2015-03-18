305cd2cb5532272493edd89d321dc0cb79d1a42c
jimroll <jroll@bgsu.edu>
2015-03-18 19:38:27 -0400

    Update alignment page

--- JAR3Doutput/JAR3Dresults/views.py
+++ JAR3Doutput/JAR3Dresults/views.py
@@ -105,7 +105,7 @@ def single_result(request,uuid,loopid,motifgroup):
         line_base = 'Sequence_' + str(res.seq_id)
         for corr_line in corrs:
             seq = Query_sequences.objects.filter(query_id = uuid, seq_id = res.seq_id, loop_id = loopid)[0].loop_sequence
-            rotation = Results_by_loop.filter(query_id = uuid, loop_id = loopid, motif_id = motifgroup)[0].rotation
+            rotation = Results_by_loop.objects.filter(query_id = uuid, loop_id = loopid, motif_id = motifgroup)[0].rotation
             if rotation == 1:
                 strands = seq.split('*')
                 seq = strands[1] + '*' + strands[0]
