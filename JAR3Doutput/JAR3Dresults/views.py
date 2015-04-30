a8503fdcfa6770b15e0bb01ef7acdb8fe064c940
jimroll <jroll@bgsu.edu>
2015-04-30 18:32:19 -0400

    Update alignment page

--- JAR3Doutput/JAR3Dresults/views.py
+++ JAR3Doutput/JAR3Dresults/views.py
@@ -169,17 +169,12 @@ def single_result(request,uuid,loopid,motifgroup):
             line1 = sequencealig[key]
             key2 = 'Sequence_' + str(res2.seq_id)
             line2 = sequencealig[key2]
-            ed_line.append(str(compare_lists(line1, line2)))
+             edit = str(compare_lists(line1, line2))
+            ed_line.append((edit, color_dict.setdefault(ed, '#df8080')))
         edit_lines.append(ed_line)
     header_zip = zip(col_nums,position,insertions)
     seq_zip = zip(seq_lines, edit_lines)
     color_dict = {'0':'#ffffff', '1':'#f8eaea', '2':'#f1d4d4', '3':'#eabfbf', '4':'#e3aaaa', '5':'#dc9595'}
-    seq_colors = []
-    for eds in edit_lines:
-        colors = []
-        for ed in eds:
-            colors.append(color_dict.setdefault(ed, '#df8080'))
-        seq_colors.append(colors)
     mkeys = sorted(motifalig.keys())
     edit_lines = []
     for key in mkeys:
@@ -192,7 +187,8 @@ def single_result(request,uuid,loopid,motifgroup):
             line1 = motifalig[key]
             key2 = 'Sequence_' + str(res2.seq_id)
             line2 = sequencealig[key2]
-            ed_line.append(str(compare_lists(line1, line2)))
+            edit = str(compare_lists(line1, line2))
+            ed_line.append((edit, color_dict.setdefault(ed, '#df8080')))
         edit_lines.append(ed_line)
         motif_lines.append(line)
     motif_data = zip(motif_names,motif_lines,edit_lines)
@@ -204,16 +200,9 @@ def single_result(request,uuid,loopid,motifgroup):
         filenamewithpath = '/Users/api/Models/HL/1.13/lib/' + motifgroup + '_interactions.txt'
     with open(filenamewithpath,"r") as f:
         interaction_text = f.read().replace(' ','\t')
-    motif_colors = []
-    for eds in edit_lines:
-        colors = []
-        for ed in eds:
-            colors.append(color_dict.setdefault(ed, '#df8080'))
-        motif_colors.append(colors)
-
     return render_to_response('JAR3Doutput/base_result_loop_done.html',
                                   {'query_info': q, 'header_zip': header_zip,
-                                  'loopnum': loopid, 'motifid': motifgroup, 'colors': colors,
+                                  'loopnum': loopid, 'motifid': motifgroup,
                                   'seq_zip': seq_zip, 'motif_data': motif_data, 'seq_text': seq_text,
                                   'model_text': model_text, 'inter_text': interaction_text,
                                   'rotation': rotation}, context_instance=RequestContext(request))
