cfad77538bbfd444b5cdbe4b07979596af7ae714
Blake Sweeney <blakes.85@gmail.com>
2015-05-11 10:50:39 -0400

    Add some changes from production

--- JAR3Doutput/urls.py
+++ JAR3Doutput/urls.py
@@ -7,11 +7,11 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('',
 
-    url(r'^jar3d_dev/process_input$', 'JAR3Dresults.views.process_input'),
-    url(r'^jar3d_dev/result/(?P<uuid>\w{8}-\w{4}-\w{4}-\w{4}-\w{12})/$', 'JAR3Dresults.views.result'),
-    url(r'^jar3d_dev/result/(?P<uuid>\w{8}-\w{4}-\w{4}-\w{4}-\w{12})/(?P<loopid>\d)/(?P<motifgroup>.*)/$', 'JAR3Dresults.views.single_result'),
-    url(r'^jar3d_dev/refine/(?P<uuid>\w{8}-\w{4}-\w{4}-\w{4}-\w{12})/$', 'JAR3Dresults.views.home', name='refine'),
-    url(r'^jar3d_dev/$', 'JAR3Dresults.views.home', name='home'),
+    url(r'^process_input$', 'JAR3Dresults.views.process_input'),
+    url(r'^result/(?P<uuid>\w{8}-\w{4}-\w{4}-\w{4}-\w{12})/$', 'JAR3Dresults.views.result'),
+    url(r'^result/(?P<uuid>\w{8}-\w{4}-\w{4}-\w{4}-\w{12})/(?P<loopid>\d)/(?P<motifgroup>.*)/$', 'JAR3Dresults.views.single_result'),
+    url(r'^refine/(?P<uuid>\w{8}-\w{4}-\w{4}-\w{4}-\w{12})/$', 'JAR3Dresults.views.home', name='refine'),
+    url(r'^$', 'JAR3Dresults.views.home', name='home'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
