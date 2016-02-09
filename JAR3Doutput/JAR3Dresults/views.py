bc25f4e21b99b74a39fe8f17e01a5b60ac3b1b36
jimroll <jroll@bgsu.edu>
2016-02-09 14:55:00 -0500

    Filter query results for distinct values

--- JAR3Doutput/JAR3Dresults/views.py
+++ JAR3Doutput/JAR3Dresults/views.py
@@ -92,7 +92,7 @@ def result(request, uuid):
                                   context_instance=RequestContext(request))
 
 def single_result(request,uuid,loopid,motifgroup):
-    q = Loop_query_info.objects.filter(query_id=uuid, loop_id=loopid, motif_group=motifgroup).distinct()
+    q = Loop_query_info.objects.filter(query_id=uuid, loop_id=loopid, motif_group=motifgroup)
     group_set = Query_info.objects.filter(query_id=uuid)[0].group_set
     rows = []
     if q:
@@ -114,7 +114,7 @@ def single_result(request,uuid,loopid,motifgroup):
                                   {'query_info': q,
                                   'loopnum': loopid, 'motifid': motifgroup},
                                   context_instance=RequestContext(request))
-    seq_res = Results_by_loop_instance.objects.filter(query_id=uuid).filter(loop_id=loopid).filter(motif_id=motifgroup).order_by('seq_id')
+    seq_res = Results_by_loop_instance.objects.filter(query_id=uuid).filter(loop_id=loopid).filter(motif_id=motifgroup).order_by('seq_id').distinct()
     rotation = Results_by_loop.objects.filter(query_id = uuid, loop_id = loopid, motif_id = motifgroup)[0].rotation
     for indx, res in enumerate(seq_res):
         corrs = Correspondence_results.objects.filter(result_instance_id = res.id)
