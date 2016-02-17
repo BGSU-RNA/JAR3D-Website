a025fa83ff0e157644c3e87e954a0735850e6681
jimroll <jroll@bgsu.edu>
2016-02-17 14:11:03 -0500

    Fix for repeated rows

--- JAR3Doutput/JAR3Dresults/views.py
+++ JAR3Doutput/JAR3Dresults/views.py
@@ -114,8 +114,13 @@ def single_result(request,uuid,loopid,motifgroup):
                                   {'query_info': q,
                                   'loopnum': loopid, 'motifid': motifgroup},
                                   context_instance=RequestContext(request))
-    seq_res = Results_by_loop_instance.objects.filter(query_id=uuid).filter(loop_id=loopid).filter(motif_id=motifgroup).order_by('seq_id') \
-                .values('query_id','seq_id','loop_id','motif_id','cutoff','score','cutoff_score','interioreditdist','fulleditdist','rotation').distinct()
+    seq_res_rep = Results_by_loop_instance.objects.filter(query_id=uuid).filter(loop_id=loopid).filter(motif_id=motifgroup).order_by('seq_id')
+    seq_res = []
+    ids = set()
+    for res in seq_res_rep:
+        if res.seq_id not in ids:
+            seq_res.append(res)
+            ids.add(res.seq_id)
     rotation = Results_by_loop.objects.filter(query_id = uuid, loop_id = loopid, motif_id = motifgroup)[0].rotation
     for res in seq_res:
         corrs = Correspondence_results.objects.filter(result_instance_id = res.id)
