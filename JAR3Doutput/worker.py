4de9a35b9b84a27ba5340d39e1b741ec2c8d0a34
Blake Sweeney <blakes.85@gmail.com>
2016-02-18 16:53:26 -0500

    First attempt at new beanstalk based queue
    This begins the process of adding a beanstalk queue to the webserver.
    The current queue is based on perl and is causing issues with running
    too long and taking up too much memory. I'm not sure why this is
    happening.
    
    I add 2 workers, one for the scoring of an input (bin/score.py) and one
    for the aligning of a loop to a model (bin/align.py). These are based
    off the generic worker class in JAR3Doutput/worker.py which is a
    modification of the worker class from r3d-2-msa. I'm planning on using
    subprocess32 as this improves the subprocess in python 2.6 (our current
    version). It notably adds timeout support, something we need. The code
    is untested right now but this is an outline of what I will attempt to
    write.
    
    Once this is done we will remove the queue/ directory entirely.

---
+++ JAR3Doutput/worker.py
@@ -0,0 +1,87 @@
+"""A module that contains useful tools for implementing background workers.
+"""
+
+import abc
+
+import json
+import beanstalkc
+
+
+class Worker(object):
+    """A base class for all workers. This will pull jobs from the beanstalk
+    queue. It does not yet update the databaase with pending status and the
+    like but could in the future. Currently it only logs what it is doing. To
+    use this it should be subclassed and the process method should be
+    implemented. This will ignore the result of the process method as the
+    process is expected to save things itself.
+    """
+
+    def __init__(self, config, **kwargs):
+        """Create a new Worker. This uses the config dictionary to connect to
+        the queue and worker. That dictionary must contain a cache dictionary
+        with the keys connection to indicate the connection to use, timeout for
+        the time in seconds until an object is expired in the cache.
+
+        :config: A configuration dictionary. This must specify the cache and
+        queue to use.
+        :kwargs: Keyword arguments to set as attributes of this worker. If no
+        name is given then a random UUID is generated as a name.
+        """
+        self.config = dict(config)
+        self.worker = self.config['worker']
+        self.beanstalk = beanstalkc.Connection(**config['queue']['connection'])
+        self.beanstalk.watch(config['worker']['queue'])
+        self.beanstalk.ignore('default')
+
+        self.name = kwargs.get('name', str(uuid.uuid4()))
+        self.logger = logging.getLogger('queue.Worker:%s' % self.name)
+
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    @abc.abstractmethod
+    def process(self, query):
+        """Process the data. This method takes the input query which was placed
+        in the queue and preforms some operation to create the result data
+        structure. In addition, the state of the job will be updated. If this
+        process fails it should raise an exception to indicate failure. All
+        worker subclasses must implement this.
+
+        :param dict query: The query to process.
+        :returns: Nothing.
+        """
+        pass
+
+    def work(self, job, query):
+        """Work on a job. This will work on some job until it is finished. The
+        job will be deleted from the queue if this finishes. The job's status
+        will be set to pending while it is being processed and then to success
+        if it succeeds.
+
+        :param Job job: The job to work on.
+        :param dict query: The query to work on.
+        """
+
+        self.logger.debug("Working on query %s", query['id'])
+        result = self.process(query)
+        self.logger.debug("Done working on %s", query['id'])
+
+    def __call__(self):
+        """The main entry point for all workers. When called this will wait
+        until a job can be reserved from the queue and then send it to
+        self.work(). The jobs body's are expected to be JSON encoded
+        dictionaries. 
+        """
+
+        self.logger.info("Starting worker %s", self.name)
+        while True:
+            job = self.beanstalk.reserve()
+            job.bury()
+            try:
+                query = json.loads(job.body)
+                self.work(job, query)
+            except Exception as err:
+                self.logger.error("Error working with %s", query['id'])
+                self.logger.exception(err)
+            finally:
+                job.delete()
